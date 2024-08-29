05528a20f12e1c215fab1ec6444722c160e7aa24
Teoh TeikToe <teohteiktoe@gmail.com>
2024-08-29 11:51:27 +0000

    1

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/store_money',methods=["get","post"])
+def store_money():
+    return render_template('store_money.html')
+
+@app.route('/transfer_money',methods=["get","post"])
+def transfer_money():
+    return render_template('transfer_money.html')
+
 if __name__=='__main__':
     app.run()

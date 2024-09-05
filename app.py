aa8609616de4df115d2c549a0fe0dba494b1da8f
Teoh TeikToe <teohteiktoe@gmail.com>
2024-09-05 11:36:16 +0000

    1

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/main',methods=["get","post"])
+def main():
+    r = request.form.get("q")
+    return render_template('main.html',r=r)
+
 @app.route('/store_money',methods=["get","post"])
 def store_money():
     return render_template('store_money.html')
